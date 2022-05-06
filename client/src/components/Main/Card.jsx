4fe80bf18ebf73fe12e9de3bd807f1beb8196477
seejiahao <jhsee11@gmail.com>
2022-05-06 23:13:56 +0800

    leggo123

--- client/src/components/Main/Card.jsx
+++ client/src/components/Main/Card.jsx
@@ -2,22 +2,22 @@ import React from 'react';
 
 const Card = (props) => {
   return (
-    <div class="flow-root">
-      <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
-        <li class="py-3 sm:py-4">
-          <div class="flex items-center space-x-4">
-            <div class="flex-shrink-0">
+    <div className="flow-root">
+      <ul role="list" className="divide-y divide-gray-200 dark:divide-gray-700">
+        <li className="p-4 border">
+          <div className="flex justify-between items-center space-x-4">
+            <div className="w-24">
               <p>{props.category}</p>
             </div>
-            <div class="flex-1 min-w-0">
-              <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+            <div className="">
+              <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
                 {props.note}
               </p>
-              <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+              <p className="text-sm text-gray-500 truncate dark:text-gray-400">
                 {props.account}
               </p>
             </div>
-            <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+            <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
               {props.amount}
             </div>
           </div>
--- client/src/components/Main/DailyExpense.jsx
+++ client/src/components/Main/DailyExpense.jsx
@@ -3,58 +3,57 @@ import Card from './Card';
 //import data from './TransactionApi';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
+import { format } from 'date-fns';
 
 const DailyExpense = () => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    //let cancel = false;
-
     axios.get('http://localhost:5001/api/transaction').then((response) => {
-      //if (cancel) return;
-
       console.log(`response is ${JSON.stringify(response.data)}`);
       console.log(`type is ${typeof data}`);
       setData(response.data);
 
       console.log(`data is ${JSON.stringify(data)}`);
     });
-
-    //   return () => {
-    //     cancel = true;
-    //   };
   }, []);
 
   return (
-    <div>
-      {data.map((val, index) => {
-        return (
-          <div class="p-4 max-w-md bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-            <div class="flex justify-between items-center mb-4">
-              <h5 class="text-xl font-bold leading-none text-gray-900 dark:text-white">
-                {val.date}
-              </h5>
-              <a
-                href="#"
-                class="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500"
-              >
-                View all
-              </a>
+    <div className="flex justify-center">
+      <div className="w-2/5 my-8 m-6 p-6">
+        {data.map((val, index) => {
+          return (
+            <div
+              key={index}
+              className="p-4 matailwx-w-md bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700"
+            >
+              <div className="flex justify-between items-center mb-4">
+                <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">
+                  {new Date(val.date).toLocaleDateString()}
+                </h5>
+                <a
+                  href="#"
+                  className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500"
+                >
+                  View all
+                </a>
+              </div>
+              {val.items.map((item, index) => {
+                return (
+                  <ul key={index}>
+                    <Card
+                      category={item.category}
+                      note={item.note}
+                      account={item.account}
+                      amount={item.amount}
+                    />
+                  </ul>
+                );
+              })}
             </div>
-            {val.items.map((item, index) => {
-              return (
-                <Card
-                  key={index}
-                  category={item.category}
-                  note={item.note}
-                  account={item.account}
-                  amount={item.amount}
-                />
-              );
-            })}
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 };
